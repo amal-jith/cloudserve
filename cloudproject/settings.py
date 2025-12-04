3156302f1e5b3075582eaabcaaaabc5ac0231ab1
amal_jith <123450863+amal-jith@users.noreply.github.com>
2025-12-04 16:11:04 +0530

    Update settings.py

--- cloudproject/settings.py
+++ cloudproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%92m_-%h58@*%bkp^pian+p2ib&susuj%gqa!dbh$!_2d-2m3o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
